e2348c2284b45853321f837d11b97bb80f6d6791
evie-8 <neouma29@gmail.com>
2024-03-17 16:19:53 +0300

    corrections

--- components/dashboard/SideNavBar.tsx
+++ components/dashboard/SideNavBar.tsx
@@ -95,7 +95,7 @@ const SideNavBar = ({
                             
         </div>
     </div>
-    <div>
+    <div className='flex-1 overflow-y-auto'>
         {children}
     </div>
   </section>
